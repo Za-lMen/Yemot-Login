a14245de377b3b0dc47c58c6eb5d9260f899caf8
Za-lMen <6727402@gmail.com>
2025-09-30 20:39:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,42 @@
 from flask import Flask, request
 import os
+import traceback
 app = Flask(__name__)
 
 @app.route('/')
 
 def login():
-    num = request.args.get('num')
-    pas = request.args.get('pas')
-    mfaid = request.args.get('mfaid')
-    baseurl = 'https://www.call2all.co.il/ym/api/'
-    logincon = 'Login'
-    numurl = f'username={num}'
-    pasurl = f'password={pas}'
-    err = ''
+	num = request.args.get('num')
+	pas = request.args.get('pas')
+	mfaid = request.args.get('mfaid')
+	baseurl = 'https://www.call2all.co.il/ym/api/'
+	logincon = 'Login'
+	numurl = f'username={num}'
+	pasurl = f'password={pas}'
+	err = ''
 
-    if not num:
-        err += 'no num param. '
+	if not num:
+		err += 'no num param. '
+	if not pas:
+		err += 'no pas param. '
+	if not mfaid:
+		err += 'no mfaid param.'
+	if err:
+		return err.strip()
 
-    if not pas:
-        err += 'no pas param. '
+	urllogin = f'{baseurl}{logincon}?{numurl}&{pasurl}'
+	
+	try:
+		reslogin = requests.get(urllogin)
+		datlogin = reslogin.json()
+		token = datlogin.get('token')
+		if token:
+			return token
+		return 'No Token Received'
 
-    if not mfaid:
-        err  += 'no mfaid param.'
+except Exception as e:
+	bug = f"Error type: {type(e).__name__}\nError message: {str(e)}\nTraceback: {traceback.format_exc()}"
+	return bug
 
-    if err:
-        return err.strip()
-
-    urllogin = f'{baseurl}{logincon}?{numurl}&{pasurl}'
-    
-    try:
-        reslogin = requests.get(urllogin)
-        datlogin = reslogin.json()
-#        token = datlogin.get('token')
-        token = str(datlogin)
-        if token:
-            return token
-        return 'No Token Received'
-
-    except:
-        return 'no token received'
-
+#	except:
+		return 'no token received'
