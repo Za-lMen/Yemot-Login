d9dd4f06d3e85d624ad7ecde65496b3cb6fa03bf
Za-lMen <6727402@gmail.com>
2025-09-30 18:22:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,25 @@ app = Flask(__name__)
 @app.route('/')
 
 def login():
-    num = request.args.get('num', 'No num param')
-    pas = request.args.get('pas', 'No pas param')
-    mfaid = request.args.get('mfaid', 'No mfaid param')
+    num = request.args.get('num')
+    pas = request.args.get('pas')
+    mfaid = request.args.get('mfaid')
     baseurl = 'https://www.call2all.co.il/ym/api/'
     loginurl = 'Login'
     numurl = f'username={num}'
     pasurl = f'password={pas}'
+    err = ''
+
+    if not num:
+        err += 'no num param. '
+
+    if not pas:
+        err += 'no pas param. '
+
+    if not mfaid:
+        err  += 'no mfaid param.'
+
+    if err:
+        return err.strip()
+
     return f'{baseurl}{loginurl}?{numurl}&{pasurl}'
